94ab470abb658a0d2b58eded1ee1101a6467920c
pluis9 <luispalcivar@gmail.com>
2022-05-31 08:19:22 -0500

    nuevos controladores

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 
-from flask import Flask, render_template
+from markupsafe import escape
+
+from flask import Flask, render_template, request, flash, url_for
 
 app=Flask(__name__, template_folder='templates')
 
@@ -10,6 +12,65 @@ app=Flask(__name__, template_folder='templates')
 #funcion que renderiza la pagina index.html
 def inicio():
     return render_template('index.html')
+
+#ruta de la pagina registro
+@app.route('/registro', methods=['POST'])
+
+#funcion que renderiza la pagina estadocliente.html
+def registro():
+    return render_template('estadocliente.html')
+
+#ruta de la pagina registro
+@app.route('/enviar', methods=['POST'])
+
+#funcion que renderiza la pagina estadocliente.html
+def enviar():
+    nombre=request.form.get('nombre')
+    telefono=request.form.get('telefono')
+    estado=request.form.get('estado')
+
+    nombres=[]
+    telefonos=[]
+    estados=[]
+
+    nombres.append(nombre)
+    telefonos.append(telefono)
+    estados.append(estado)
+
+    return render_template('estadocliente.html', nombres=nombres, telefonos=telefonos, estados=estados)
+
+#ruta de la pagina index
+@app.route('/tienda', methods=['POST'])
+
+#funcion que renderiza la pagina tienda.html
+def registro():
+    return render_template('tienda.html')
+
+#ruta de la pagina index
+@app.route('/enviar_tienda', methods=['POST'])
+
+#funcion que renderiza la pagina tienda.html
+def enviarTienda():
+    nombre=request.form.get('nombre')
+    telefono=request.form.get('telefono')
+    estado=request.form.get('estado')
+    gerente=request.form.get('gerente')
+
+    nombres=[]
+    telefonos=[]
+    estados=[]
+    gerentes=[]
+
+    nombres.append(nombre)
+    telefonos.append(telefono)
+    estados.append(estado)
+    gerentes.append(gerente)
+
+    return render_template('tienda.html', nombres=nombres, telefonos=telefonos, estados=estados, gerentes=gerentes)
+
+
+
+
 
 #metodo principal
 if __name__=='__main__':
