df230bfa90ed7264a91011f796d515e2d4f912c2
pluis9 <luispalcivar@gmail.com>
2022-05-31 07:50:34 -0500

    se agregan formularios

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ from flask import Flask, render_template
 
 app=Flask(__name__, template_folder='templates')
 
+
+#ruta de la pagina index
 @app.route('/')
 
+#funcion que renderiza la pagina index.html
 def inicio():
     return render_template('index.html')
 
-
+#metodo principal
 if __name__=='__main__':
     app.run(debug=True)
+
 
 
 
